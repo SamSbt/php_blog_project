ea73b5f82ce4e013c5f6a88a09c5005e7b4a7037
Sam Sabat <samnsabat@gmail.com>
2024-07-25 18:28:55 +0200

    hot fix config

--- api/index.php
+++ api/index.php
@@ -2,7 +2,8 @@
 
 use Controller\Router;
 
-header("Access-Control-Allow-Origin: *"); 
+$origin = "http://localhost:5173";
+header("Access-Control-Allow-Origin: $origin"); 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); 
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
